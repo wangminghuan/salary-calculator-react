d971d9649478ad4154d2e3dc09e4ca963862f25e
wangminghuan <m.h.wang@foxmail.com>
2017-07-23 12:35:27 +0800

    增加城市切换组件

---
+++ app/resource/components/CalItemCity.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import "./CalItemCity.scss";
+
+export default class extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    
+  }
+}
