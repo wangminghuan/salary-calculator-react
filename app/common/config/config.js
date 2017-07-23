66f5ea0247717ed28121f45514aced5834542d4f
wangminghuan <m.h.wang@foxmail.com>
2017-07-23 16:26:49 +0800

    完善配置文件

--- app/common/config/config.js
+++ app/common/config/config.js
@@ -5,80 +5,597 @@
 生育保险（maternity insurance）
 住房公积金（Housing Provident Fund）*/
 
-export default [
+export default[
   {
-    "name": "北京",
-    "dirname": "bj",
-    "id": "1",
-    "housing": {
+    "name" : "北京",
+    "dirname" : "bj",
+    "id" : "1",
+    "housing" : {
       "company": "12%",
       "person": "12%"
     },
-    "endowment": {
+    "endowment" : {
       "company": "20%",
       "person": "8%"
     },
-    "medical": {
+    "medical" : {
       "company": "10%",
       "person": "2%"
     },
-    "unemployment": {
+    "unemployment" : {
       "company": "1%",
       "person": "0.2%"
     },
-    "injury": {
+    "injury" : {
       "company": "0.3%",
       "person": "0"
     },
-    "maternity": {
+    "maternity" : {
       "company": "0.8%",
       "person": "0"
     },
-    "socialNum": {
+    "socialNum" : {
       "max": "21258",
       "min": "2834"
     },
-    "houseNum": {
+    "houseNum" : {
       "max": "21258",
       "min": "1955"
     }
-  },
-  {
-    "name": "上海",
-    "dirname": "sh",
-    "id": "2",
-    "housing": {
+  }, {
+    "name" : "上海",
+    "dirname" : "sh",
+    "id" : "2",
+    "housing" : {
       "company": "7%",
       "person": "12%"
     },
-    "endowment": {
+    "endowment" : {
       "company": "22%",
       "person": "8%"
     },
-    "medical": {
+    "medical" : {
       "company": "12%",
       "person": "2%"
     },
-    "unemployment": {
+    "unemployment" : {
       "company": "2%",
       "person": "0.2%"
     },
-    "injury": {
+    "injury" : {
       "company": "0.5%",
       "person": "0"
     },
-    "maternity": {
+    "maternity" : {
       "company": "0.5%",
       "person": "0"
     },
 
-    "socialNum": {
+    "socialNum" : {
       "max": "17817",
       "min": "3653"
     },
-    "houseNum": {
+    "houseNum" : {
       "max": "17817",
       "min": "2020"
+    }
+  }, {
+    "name" : "深圳",
+    "id" : "4",
+    "dirname" : "sz",
+    "housing" : {
+      "company": "13%",
+      "person": "13%"
+    },
+    "endowment" : {
+      "company": "14%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "6.5%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "2%",
+      "person": "1%"
+    },
+    "injury" : {
+      "company": "0.8%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "12615",
+      "min": "2336"
+    },
+    "houseNum" : {
+      "max": "24588",
+      "min": "1500"
+    }
+  }, {
+    "name" : "广州",
+    "id" : "3",
+    "dirname" : "gz",
+    "housing" : {
+      "company": "13%",
+      "person": "13%"
+    },
+    "endowment" : {
+      "company": "14%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "6.5%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "2%",
+      "person": "1%"
+    },
+    "injury" : {
+      "company": "0.8%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "12303",
+      "min": "2461"
+    },
+    "houseNum" : {
+      "max": "26565",
+      "min": "1550"
+    }
+  }, {
+    "name" : "天津",
+    "id" : "18",
+    "dirname" : "tj",
+    "housing" : {
+      "company": "11%",
+      "person": "11%"
+    },
+    "endowment" : {
+      "company": "20%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "10%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "2%",
+      "person": "1%"
+    },
+    "injury" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.8%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "9380",
+      "min": "1720"
+    },
+    "houseNum" : {
+      "max": "9380",
+      "min": "1160"
+    }
+  }, {
+    "name" : "南京",
+    "id" : "172",
+    "dirname" : "nj",
+    "housing" : {
+      "company": "8%",
+      "person": "8%"
+    },
+    "endowment" : {
+      "company": "20%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "9%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "1%",
+      "person": "0.5%"
+    },
+    "injury" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "16200",
+      "min": "2628"
+    },
+    "houseNum" : {
+      "max": "18200",
+      "min": "1770"
+    }
+  }, {
+    "name" : "武汉",
+    "id" : "158",
+    "dirname" : "wh",
+    "housing" : {
+      "company": "8%",
+      "person": "8%"
+    },
+    "endowment" : {
+      "company": "20%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "8%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "1.5%",
+      "person": "0.5%"
+    },
+    "injury" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.7%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "12994.8",
+      "min": "2599"
+    },
+    "houseNum" : {
+      "max": "22733.75",
+      "min": "1300"
+    }
+  }, {
+    "name" : "杭州",
+    "id" : "79",
+    "dirname" : "hz",
+    "housing" : {
+      "company": "12%",
+      "person": "12%"
+    },
+    "endowment" : {
+      "company": "14%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "11.5%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "1.5%",
+      "person": "0.5%"
+    },
+    "injury" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "1.2%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "12093",
+      "min": "2418.6"
+    },
+    "houseNum" : {
+      "max": "17302",
+      "min": "1860"
+    }
+  }, {
+    "name" : "重庆",
+    "id" : "37",
+    "dirname" : "cq",
+    "housing" : {
+      "company": "7%",
+      "person": "7%"
+    },
+    "endowment" : {
+      "company": "18%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "6%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "1%",
+      "person": "1%"
+    },
+    "injury" : {
+      "company": "0.6%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.6%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "8832",
+      "min": "1178"
+    },
+    "houseNum" : {
+      "max": "8832",
+      "min": "0"
+    }
+  }, {
+    "name" : "济南",
+    "id" : "265",
+    "dirname" : "jn",
+    "housing" : {
+      "company": "8%",
+      "person": "8%"
+    },
+    "endowment" : {
+      "company": "20%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "8%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "2%",
+      "person": "1%"
+    },
+    "injury" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.8%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "7776",
+      "min": "1556"
+    },
+    "houseNum" : {
+      "max": "7776",
+      "min": "0"
+    }
+  }, {
+    "name" : "西安",
+    "id" : "483",
+    "dirname" : "xa",
+    "housing" : {
+      "company": "8%",
+      "person": "8%"
+    },
+    "endowment" : {
+      "company": "20%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "7%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "2%",
+      "person": "1%"
+    },
+    "injury" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "7572",
+      "min": "1514.4"
+    },
+    "houseNum" : {
+      "max": "7572",
+      "min": "0"
+    }
+  }, {
+    "name" : "成都",
+    "id" : "102",
+    "dirname" : "cd",
+    "housing" : {
+      "company": "7%",
+      "person": "7%"
+    },
+    "endowment" : {
+      "company": "20%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "7.5%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "2%",
+      "person": "1%"
+    },
+    "injury" : {
+      "company": "0.6%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.6%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "6819",
+      "min": "1364"
+    },
+    "houseNum" : {
+      "max": "6819",
+      "min": "0"
+    }
+  }, {
+    "name" : "苏州",
+    "id" : "5",
+    "dirname" : "su",
+    "housing" : {
+      "company": "8%",
+      "person": "8%"
+    },
+    "endowment" : {
+      "company": "20%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "8%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "2%",
+      "person": "1%"
+    },
+    "injury" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "1%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "16208",
+      "min": "2387"
+    },
+    "houseNum" : {
+      "max": "15440",
+      "min": "0"
+    }
+  }, {
+    "name" : "南昌",
+    "id" : "669",
+    "dirname" : "nc",
+    "housing" : {
+      "company": "8%",
+      "person": "8%"
+    },
+    "endowment" : {
+      "company": "20%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "6%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "2%",
+      "person": "1%"
+    },
+    "injury" : {
+      "company": "0.4%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.8%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "7611",
+      "min": "1522.2"
+    },
+    "houseNum" : {
+      "max": "7611",
+      "min": "0"
+    }
+  }, {
+    "name" : "太原",
+    "id" : "740",
+    "dirname" : "ty",
+    "housing" : {
+      "company": "10%",
+      "person": "6%"
+    },
+    "endowment" : {
+      "company": "20%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "6.5%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "1.5%",
+      "person": "0.5%"
+    },
+    "injury" : {
+      "company": "0.6%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "3873",
+      "min": "774.6"
+    },
+    "houseNum" : {
+      "max": "3873",
+      "min": "774.6"
+    }
+  }, {
+    "name" : "石家庄",
+    "id" : "265",
+    "dirname" : "sjz",
+    "housing" : {
+      "company": "11%",
+      "person": "7%"
+    },
+    "endowment" : {
+      "company": "20%",
+      "person": "8%"
+    },
+    "medical" : {
+      "company": "8%",
+      "person": "2%"
+    },
+    "unemployment" : {
+      "company": "2%",
+      "person": "1%"
+    },
+    "injury" : {
+      "company": "0.5%",
+      "person": "0"
+    },
+    "maternity" : {
+      "company": "0.8%",
+      "person": "0"
+    },
+
+    "socialNum" : {
+      "max": "7095",
+      "min": "1419"
+    },
+    "houseNum" : {
+      "max": "7095",
+      "min": "0"
     }
   }
 ]
